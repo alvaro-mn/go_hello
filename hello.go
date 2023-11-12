6ac44fe90589ad987bc5fac6e1c7e002194e55ab
Alvaro <almunav@gmail.com>
2023-11-12 15:27:33 +0100

    test

--- hello.go
+++ hello.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"net/http"
+)
+
+const (
+	apiKey    = "ed070817-20b0-490e-a11a-aaa186a0df49"
+	apiSecret = "0c4ead76-4ab6-47f8-921c-de00b2bbfbc5"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Hello, your key is: %s\n", apiKey)
+	})
+
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		user := r.Form.Get("user")
+		pw := r.Form.Get("password")
+
+		slog.Info("Registering new user %s with password %s.\n", user, pw)
 	})
 
 	err := http.ListenAndServe(":8080", nil)
